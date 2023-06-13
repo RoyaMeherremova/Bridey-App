3ff74ea354b57785bfa7d974f67b2b3364b35c6b
unknown <royaam@code.edu.az>
2023-06-13 12:33:49 +0400

    changes added

--- assets/js/productDetail.js
+++ assets/js/productDetail.js
@@ -1,17 +1,48 @@
 $(document).ready(function () {
-    $('.slick', '.vertical-slider').slick({
-        vertical: true,
-        verticalSwiping: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        slidesToShow: 4,
-        slidesToScroll: 1,
 
-        prevArrow: $('.prev'),
-        nextArrow: $('.next')
+    $('ul.tabs li').click(function () {
+        var tab_id = $(this).attr('data-tab');
 
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
 
+        $(this).addClass('current');
+        $("#" + tab_id).addClass('current');
+    })
 
-    });
+})
+$('.prod-carousel').slick({
+    dots: true,
+    infinite: false,
+    speed: 900,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    autoplay: true,
+    autoplayspeed: 1000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
 
+    ]
 });

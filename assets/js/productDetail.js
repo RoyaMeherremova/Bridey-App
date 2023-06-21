aea383ec149e897d255ff9c1f8f8eca66972c0bf
unknown <royaam@code.edu.az>
2023-06-21 15:54:35 +0400

    responsive is ready

--- assets/js/productDetail.js
+++ assets/js/productDetail.js
@@ -11,38 +11,48 @@ $(document).ready(function () {
     })
 
 })
+
+
+//relatedProduct-cards-slider
 $('.prod-carousel').slick({
     dots: true,
     infinite: false,
-    speed: 900,
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    autoplay: true,
-    autoplayspeed: 1000,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 4,
     responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
         }
-
+      },
+      {
+        breakpoint: 920,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+  
     ]
-});
+  });
+  
+  
